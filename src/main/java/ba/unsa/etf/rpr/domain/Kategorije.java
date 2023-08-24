b069e01ad5a759e0fef04c55b23724d7d02d71bd
shasic2 <shasic2@etf.unsa.ba>
2023-08-25 00:10:28 +0200

    klasa Kategorije zavrsena

--- src/main/java/ba/unsa/etf/rpr/domain/Kategorije.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Kategorije.java
@@ -1,5 +1,52 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Kategorije implements  Idable{
+import java.util.Objects;
 
+public class Kategorije implements  Idable{
+  private int kategorija_id;
+  private String ime;
+
+    public Kategorije(int kategorija_id, String ime) {
+        this.kategorija_id = kategorija_id;
+        this.ime = ime;
+    }
+
+    public Kategorije(){}
+
+    public int getKategorija_id() {
+        return kategorija_id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setKategorija_id(int kategorija_id) {
+        this.kategorija_id = kategorija_id;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    @Override
+    public String toString() {
+        return "Kategorije{" +
+                "kategorija_id=" + kategorija_id +
+                ", ime='" + ime + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kategorije that = (Kategorije) o;
+        return kategorija_id == that.kategorija_id && Objects.equals(ime, that.ime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(kategorija_id, ime);
+    }
 }
