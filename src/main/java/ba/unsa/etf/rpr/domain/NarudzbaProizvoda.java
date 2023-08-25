1bb977034166c76f856f388af811537465409741
shasic2 <shasic2@etf.unsa.ba>
2023-08-25 16:32:19 +0200

    Klasa NarudzbaProizvoda zavrsena

--- src/main/java/ba/unsa/etf/rpr/domain/NarudzbaProizvoda.java
+++ src/main/java/ba/unsa/etf/rpr/domain/NarudzbaProizvoda.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.domain;
 
 public class NarudzbaProizvoda implements  Idable {
+
+    private int narudzbaProizvoda_id;
+    private Proizvodi proizvod_id;
+    private Narudzba narudzba_id;
+
+
+    public int getNarudzbaProizvoda_id() {
+        return narudzbaProizvoda_id;
+    }
+
+    public Proizvodi getProizvod_id() {
+        return proizvod_id;
+    }
+
+    public Narudzba getNarudzba_id() {
+        return narudzba_id;
+    }
+
+    public void setNarudzbaProizvoda_id(int narudzbaProizvoda_id) {
+        this.narudzbaProizvoda_id = narudzbaProizvoda_id;
+    }
+
+    public void setProizvod_id(Proizvodi proizvod_id) {
+        this.proizvod_id = proizvod_id;
+    }
+
+    public void setNarudzba_id(Narudzba narudzba_id) {
+        this.narudzba_id = narudzba_id;
+    }
 }
