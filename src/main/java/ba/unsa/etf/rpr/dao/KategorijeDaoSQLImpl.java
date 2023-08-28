45c33051d926e25f5a9b79b71ce1f115285ee111
shasic2 <shasic2@etf.unsa.ba>
2023-08-28 15:14:42 +0200

    implementacija KategorijeDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijeDaoSQLImpl.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Kategorije;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class KategorijeDaoSQLImpl extends KategorijeDao {
+
+    private Connection konekcija;
+
+    public KategorijeDaoSQLImpl(){
+        try{
+            this.konekcija = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_Baza/RPR/2022/2023?sessionVariables=WAIT_TIMEOUT=2880", "freedb_shasic2", "vCpugK8F*!J*W4&");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Kategorije getById(int id) {
+        return null;
+    }
+
+
+    }
+
