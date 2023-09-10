04cc42f535ac9ddbe60d47534d5430cd41b3a772
shasic2 <shasic2@etf.unsa.ba>
2023-09-10 23:39:38 +0200

    implementirana metoda row2object u KategorijaDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/KategorijeDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijeDaoSQLImpl.java
@@ -4,7 +4,9 @@ import ba.unsa.etf.rpr.domain.Kategorije;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 import java.sql.*;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class KategorijeDaoSQLImpl extends AbstractDao<Kategorije> implements KategorijeDao {
 
@@ -27,12 +29,33 @@ public class KategorijeDaoSQLImpl extends AbstractDao<Kategorije> implements Kat
 
     @Override
     public Kategorije row2object(ResultSet rs) throws HealthyShopException {
-        return null;
+        try {
+            Kategorije kategorija = new Kategorije();
+            kategorija.setId(rs.getInt("id"));
+            kategorija.setIme(rs.getString("ime"));
+            return kategorija;
+        } catch (SQLException e) {
+            throw new HealthyShopException(e.getMessage(), e);
+        }
     }
 
     @Override
     public Map<String, Object> object2row(Kategorije object) {
-        return null;
+        Map<String, Object> red = new TreeMap<>();
+        red.put("id", object.getId());
+        red.put("ime", object.getIme());
+        return red;
+    }
+
+    /**
+     * Metoda za pronalazak kategorije sa proslijedjenim imenom
+     * @param ime
+     * @return sve kategorije sa tim imenom
+     * @throws HealthyShopException
+     */
+    @Override
+    public List<Kategorije> pronadjiKategorijuPoID(String ime) throws HealthyShopException {
+        return executeQuery("SELECT * FROM Kategorije WHERE ime = ?", new Object[]{ime});
     }
 
 
