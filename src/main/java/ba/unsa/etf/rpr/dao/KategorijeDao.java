83345629c681a0165f548c5e4b10c84681f29d4a
shasic2 <shasic2@etf.unsa.ba>
2023-09-10 17:33:54 +0200

    Izbrisan sadrzaj KategorijeDao

--- src/main/java/ba/unsa/etf/rpr/dao/KategorijeDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijeDao.java
@@ -2,6 +2,6 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Kategorije;
 
-public abstract class KategorijeDao {
-    public abstract Kategorije getById(int id);
+public interface KategorijeDao  {
+
 }
