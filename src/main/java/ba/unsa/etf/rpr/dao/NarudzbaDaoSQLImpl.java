2d73e282deb8cfc2c2d227f8043f4096010a4a9f
shasic2 <shasic2@etf.unsa.ba>
2023-09-11 00:28:08 +0200

    implementacija NarudzbaDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
@@ -1,8 +1,24 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 import java.sql.*;
+import java.util.Map;
 
-public class NarudzbaDaoSQLImpl   {
+public class NarudzbaDaoSQLImpl extends  AbstractDao<Narudzba> implements NarudzbaDao  {
+
+    public NarudzbaDaoSQLImpl(String tableName) {
+        super(tableName);
+    }
+
+    @Override
+    public Narudzba row2object(ResultSet rs) throws HealthyShopException {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Narudzba object) {
+        return null;
+    }
 }
