ca2152f3af4b26c612e4a09cc57df646394527cd
shasic2 <shasic2@etf.unsa.ba>
2023-09-12 17:02:55 +0200

    zavrseno

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDao.java
@@ -1,7 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
-public abstract class NarudzbaDao {
+import java.util.List;
 
+public  interface NarudzbaDao {
+
+    List<Narudzba> pronadjiPoID(int id) throws HealthyShopException;
+
+    List<Narudzba> pronadjiNarudzbuPoKorisniku(int id) throws HealthyShopException;
 }
