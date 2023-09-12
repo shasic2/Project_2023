acbdeb08d558e12eed3863a2670d63197bc111c9
shasic2 <shasic2@etf.unsa.ba>
2023-09-12 17:03:26 +0200

    zavrseno

--- src/main/java/ba/unsa/etf/rpr/dao/KategorijeDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijeDao.java
@@ -1,7 +1,17 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Kategorije;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
-public interface KategorijeDao  {
+import java.util.List;
 
+
+/**
+ * DAO interfejs za objekat Kategorija
+ *
+ * @author Selma
+ */
+public interface KategorijeDao extends  Dao<Kategorije> {
+
+    List<Kategorije> pronadjiKategorijuPoID(String ime) throws HealthyShopException;
 }
