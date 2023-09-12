55283cab8a74860a1efa09bf74548dee8b93f7a5
shasic2 <shasic2@etf.unsa.ba>
2023-09-12 14:21:39 +0200

    implementacija ProizvodiDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ProizvodiDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProizvodiDaoSQLImpl.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr.dao;
 
-public class ProizvodiDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Proizvodi;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class ProizvodiDaoSQLImpl extends AbstractDao<Proizvodi> implements ProizvodiDao  {
+
+    private static ProizvodiDaoSQLImpl instance = null;
+    public ProizvodiDaoSQLImpl() {
+        super("Proizvodi");
+    }
+
+    public static ProizvodiDaoSQLImpl getInstance(){
+        if (instance == null)
+            instance = new ProizvodiDaoSQLImpl();
+        return instance;
+    }
+    public static void removeInstance(){
+        if (instance != null)
+            instance = null;
+    }
+
+    @Override
+    public Proizvodi row2object(ResultSet rs) throws HealthyShopException, SQLException {
+     return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Proizvodi object) {
+        return null;
+    }
 }
