1f2983d169fc8cdb880120760eb71857901bcdb6
shasic2 <shasic2@etf.unsa.ba>
2023-09-12 17:02:44 +0200

    zavrseno

--- src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Korisnik;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
-public abstract class KorisnikDao {
+import java.util.List;
 
+public interface  KorisnikDao extends Dao<Korisnik>{
+
+    List<Korisnik> pronadjiKorisnikaPoEmailu(String email) throws HealthyShopException;
 }
