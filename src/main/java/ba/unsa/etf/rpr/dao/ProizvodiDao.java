8e90f5a2022027157bd5805513f1943e519733cb
shasic2 <shasic2@etf.unsa.ba>
2023-09-12 17:03:17 +0200

    zavrseno

--- src/main/java/ba/unsa/etf/rpr/dao/ProizvodiDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProizvodiDao.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
-public class ProizvodiDao {
+import ba.unsa.etf.rpr.domain.Proizvodi;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
+import java.util.List;
+
+public interface ProizvodiDao {
+    List<Proizvodi> pronadjiProizvodPoKategoriji(int id) throws HealthyShopException;
+
+    List<Proizvodi> pronadjiProizvod(String ime) throws HealthyShopException;
 }
