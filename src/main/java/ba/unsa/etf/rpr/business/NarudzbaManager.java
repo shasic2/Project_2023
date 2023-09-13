550d1459dc406f04f9c95a215521c99aeea2b41e
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 22:04:43 +0200

    metoda za brisanje narudzbe

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
 public class NarudzbaManager {
+
+    public void obrisiNarudzbu(int id) throws HealthyShopException {
+        DaoFactory.NarudzbaDao().delete(id);
+    }
 }
