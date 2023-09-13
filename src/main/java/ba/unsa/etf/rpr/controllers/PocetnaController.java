2e635e20151a63124526f7d707087f6956fef0bd
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 12:28:11 +0200

    implementirano otvaranje stranice O nama - PocetnaController

--- src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -1,4 +1,52 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.App;
+import ba.unsa.etf.rpr.domain.Korisnik;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class PocetnaController {
+    public TextField Emaild;
+    public Button BtnRegistracija;
+    public Button BtnPrijava;
+    public PasswordField PasswordId;
+    public Label GreskaId;
+
+    public void akcijaPrijave(ActionEvent actionEvent) {
+
+    }
+
+    public void OtvaranjeONama(ActionEvent actionEvent) {
+        try {
+
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/ONama.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("O nama");
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void OtvaranjePomoci(ActionEvent actionEvent) {
+
+    }
+
+    public void akcijaRegistracije(ActionEvent actionEvent) {
+    }
 }
