fe3675b3a6ea9a13f54df8ff56119cd953cff29d
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 18:51:44 +0200

    metoda brisanja kategorije

--- src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
 public class KategorijaManager {
+
+    public void obrisiKategoriju(int id) throws HealthyShopException{
+        DaoFactory.kategorijeDao().delete(id);
+    }
 }
