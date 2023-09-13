204a7c8229738a3db192d8c3e5b5e1a24d6ef64a
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 23:03:54 +0200

    metoda dobijanja svih proizvoda

--- src/main/java/ba/unsa/etf/rpr/business/ProizvodiManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ProizvodiManager.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Proizvodi;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
+import java.util.List;
+
 public class ProizvodiManager {
+    public List<Proizvodi> dajSveProizvode() throws HealthyShopException {
+        return DaoFactory.proizvodDao().getAll();
+    }
 }
