8045c1d1e6e177450af77ef8def5031222ba98a0
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 22:49:00 +0200

    zbog greske u nazivu dodan novi

---
+++ src/main/java/ba/unsa/etf/rpr/business/KategorijeManager.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Kategorije;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
+import java.util.List;
+
+public class KategorijeManager {
+    public void obrisiKategoriju(int id) throws HealthyShopException {
+        DaoFactory.kategorijeDao().delete(id);
+    }
+    public void azurirajKategoriju(Kategorije kategorije) throws HealthyShopException {
+        DaoFactory.kategorijeDao().update(kategorije);
+    }
+    public void dodajKategoriju(Kategorije kategorije) throws HealthyShopException {
+        DaoFactory.kategorijeDao().add(kategorije);
+    }
+
+    public List<Kategorije> dajSveKategorije() throws HealthyShopException{
+        return DaoFactory.kategorijeDao().getAll();
+
+    }
+
+    public int pronadjiKategorijuPoID(int id) throws HealthyShopException {
+        return DaoFactory.kategorijeDao().getById(id);
+    }
+}
