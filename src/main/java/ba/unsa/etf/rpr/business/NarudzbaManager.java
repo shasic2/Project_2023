84040a22a1c5e9f97ba10e48889e7028a8c33574
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 22:14:02 +0200

    metoda za azuriranje narudzbe

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
@@ -4,12 +4,20 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Narudzba;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
+import java.util.List;
+
 public class NarudzbaManager {
 
     public void obrisiNarudzbu(int id) throws HealthyShopException {
         DaoFactory.NarudzbaDao().delete(id);
     }
+
     public void dodajNarudbu(Narudzba narudzba) throws HealthyShopException {
         DaoFactory.NarudzbaDao().add(narudzba);
+    }
+
+
+    public void azurirajNarudzbu(Narudzba narudzba) throws HealthyShopException {
+        DaoFactory.NarudzbaDao().update(narudzba);
     }
 }
