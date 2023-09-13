519b0d0f7120adf99629bd63766e8b2e6b02a054
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 22:56:40 +0200

    metoda dobijanja svih međutabela

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodaManager.java
@@ -4,9 +4,14 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.NarudzbaProizvoda;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
+import java.util.List;
+
 public class NarudzbaProizvodaManager {
 
     public NarudzbaProizvoda dodajNP(NarudzbaProizvoda np) throws HealthyShopException {
-        return (NarudzbaProizvoda) DaoFactory.NarudzbaDao().add(np);
+        return DaoFactory.narProDao().add(np);
+    }
+    public List<NarudzbaProizvoda> dajSveNP() throws HealthyShopException {
+        return DaoFactory.narProDao().getAll();
     }
 }
