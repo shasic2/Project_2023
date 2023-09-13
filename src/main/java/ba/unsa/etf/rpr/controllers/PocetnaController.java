d2ac99510d5cbb15e6158eb927115863f2eadd3a
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 12:29:10 +0200

    implementirano otvaranje stranice pomoc - PocetnaController

--- src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -44,6 +44,19 @@ public class PocetnaController {
     }
 
     public void OtvaranjePomoci(ActionEvent actionEvent) {
+        try {
+
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/pomoc.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Pomoc");
+
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
