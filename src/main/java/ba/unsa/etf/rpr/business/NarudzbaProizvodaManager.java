230fdd2f0e04c1280e523fe1bde7b3e84e99608d
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 22:23:22 +0200

    metoda dodavanja za NarudzbaProizvoda(međutabele)

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodaManager.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.NarudzbaProizvoda;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
 public class NarudzbaProizvodaManager {
+
+    public NarudzbaProizvoda dodajNP(NarudzbaProizvoda np) throws HealthyShopException {
+        return (NarudzbaProizvoda) DaoFactory.NarudzbaDao().add(np);
+    }
 }
