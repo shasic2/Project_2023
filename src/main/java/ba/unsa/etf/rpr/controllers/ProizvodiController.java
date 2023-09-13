6742f3e7c8742038d48d70d330787fdac7f1c096
shasic2 <shasic2@etf.unsa.ba>
2023-09-14 01:36:37 +0200

    implementirana promjena boje klikom na dugme  Pomoc - ProizvodiController

--- src/main/java/ba/unsa/etf/rpr/controllers/ProizvodiController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProizvodiController.java
@@ -125,9 +125,13 @@ public class ProizvodiController {
 
 
     public void promjenaBojePomoc(MouseEvent mouseEvent) {
+        Button btn = (Button) mouseEvent.getSource();
+        btn.setStyle("-fx-background-color:  #1e381a ;");
     }
 
     public void vracanjeBojePomoc(MouseEvent mouseEvent) {
+        Button btn = (Button) mouseEvent.getSource();
+        btn.setStyle("-fx-background-color:  #9CC23E  ;");
     }
 
     public void akcijaDodajUKorpu(ActionEvent actionEvent) {
