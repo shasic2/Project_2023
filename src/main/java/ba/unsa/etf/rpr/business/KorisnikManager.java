ac3d755e811136e6c753acdd9501e30d031ddca9
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 21:42:10 +0200

    metoda pronalaska emaila po korisniku

--- src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Korisnik;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+
+import java.util.List;
+
 public class KorisnikManager {
+
+
+    public static List<Korisnik> pronadjiKorisnikaPoEmailu(String email) throws HealthyShopException {
+        return DaoFactory.KorisnikDao().pronadjiKorisnikaPoEmailu(email);
+    }
 }
