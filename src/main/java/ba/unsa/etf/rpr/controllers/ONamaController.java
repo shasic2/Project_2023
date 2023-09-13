7efa363709ff93b7a4177c75bdc2c1c3d12fbae0
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 16:32:09 +0200

    implementirano zatvaranje prozora - ONamaController

--- src/main/java/ba/unsa/etf/rpr/controllers/ONamaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ONamaController.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class ONamaController {
+    public Button ZatvoriId;
+
+    public void ZatvaranjeProzora(ActionEvent actionEvent) {
+        Stage stage =(Stage)ZatvoriId.getScene().getWindow();
+        stage.close();
+    }
+
+    public void OtvoriPomoc(ActionEvent actionEvent) {
+
+    }
+
+    public void promjenaBojePomoc(MouseEvent mouseEvent) {
+    }
+
+    public void vracanjeBojePomoc(MouseEvent mouseEvent) {
+    }
+
+    public void promjenaBrojeZatvori(MouseEvent mouseEvent) {
+    }
+
+    public void promjenaBrojeOtvori(MouseEvent mouseEvent) {
+    }
 }
