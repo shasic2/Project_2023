2f1809fcf353125150e59e6bdd5e07eee977fae7
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 15:38:12 +0200

    implementirano otvaranje stranice O nama - PomocController

--- src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
@@ -1,14 +1,37 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class PomocController {
     public Button ZatvoriID;
+    public Button btnONama;
+    public Button btnPomoc;
 
     public void ZatvaranjeProzora(ActionEvent actionEvent) {
         Stage stage =(Stage)ZatvoriID.getScene().getWindow();
         stage.close();
+    }
+
+    public void OtvaranjeONama(ActionEvent actionEvent) {
+        try {
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/ONama.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("O nama");
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
