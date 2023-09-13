da3bc4d540058bf6afcd46496da77cb25f62da00
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 18:53:13 +0200

    metoda azuriranja kategorije

--- src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
@@ -1,11 +1,15 @@
 package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Kategorije;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 public class KategorijaManager {
 
     public void obrisiKategoriju(int id) throws HealthyShopException{
         DaoFactory.kategorijeDao().delete(id);
+    }
+    public void azurirajKategoriju(Kategorije kategorije) throws HealthyShopException {
+        DaoFactory.kategorijeDao().update(kategorije);
     }
 }
