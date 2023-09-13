9ffaa952ced79809e21382ba49fe9cc38ae39940
shasic2 <shasic2@etf.unsa.ba>
2023-09-13 15:27:33 +0200

    implementirano zatvaranje prozora - PomocController

--- src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class PomocController {
+    public Button ZatvoriID;
+
+    public void ZatvaranjeProzora(ActionEvent actionEvent) {
+        Stage stage =(Stage)ZatvoriID.getScene().getWindow();
+        stage.close();
+    }
 }
