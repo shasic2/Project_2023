6691cc8249230b3f5f91ddcbef825debb235ab28
shasic2 <shasic2@etf.unsa.ba>
2023-09-14 21:12:55 +0200

    implementirano zatvaranje prozora Potvrda Narudzbe - PotvrdaNarudzbeController

--- src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaNarudzbeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaNarudzbeController.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class PotvrdaNarudzbeController {
+    public Label idLabela;
+    public Button btnZatvori;
+
+    @FXML
+    public void initialize() throws HealthyShopException {
+        Model model = Model.getInstance();
+        idLabela.setText("Hvala, " + model.getKorisnik().getIme() + " " + model.getKorisnik().getPrezime() + "!");
+    }
+
+    public void zatvaranjeProzora(ActionEvent actionEvent) {
+        Stage stage =(Stage)btnZatvori.getScene().getWindow();
+        stage.close();
+    }
+
+    public void promjenaBoje(MouseEvent mouseEvent) {
+    }
+
+    public void vracanjeBoje(MouseEvent mouseEvent) {
+    }
 }
