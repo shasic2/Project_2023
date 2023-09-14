68803cccde2aad6399b5499977fc80cf8c4a7124
shasic2 <shasic2@etf.unsa.ba>
2023-09-15 01:30:04 +0200

    Java Doc dokumentacija u KorisnikManager

--- src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
@@ -5,6 +5,11 @@ import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 import java.util.List;
+/**
+ * Sloj biznis logike za upravljanje korisnicima
+ *
+ * @author Selma
+ */
 
 public class KorisnikManager {
 
@@ -29,6 +34,17 @@ public class KorisnikManager {
 
     public static List<Korisnik> dajSveKorisnike() throws HealthyShopException {
         return DaoFactory.korisnikDao().getAll();
+    }
+
+    public void validacijaImena(String ime) throws HealthyShopException {
+        if (ime == null  || !ime.matches("^[a-zA-Z]+$"))
+            throw new HealthyShopException("Ime mora sadržavati samo slova");
+
+    }
+    public void validacijaPrezimena(String prezime) throws HealthyShopException {
+        if (prezime == null  || !prezime.matches("^[a-zA-Z]+$")) {
+            throw new HealthyShopException("Prezime mora sadržavati samo slova");
+        }
     }
 
 
