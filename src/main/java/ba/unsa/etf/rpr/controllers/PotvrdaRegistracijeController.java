ec96da0b50558ca2303eb88a779898e6c305b013
shasic2 <shasic2@etf.unsa.ba>
2023-09-14 23:57:26 +0200

    zavrsena implementacija PotvrdaRegistracijeController

--- src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaRegistracijeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaRegistracijeController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class PotvrdaRegistracijeController {
+    public Button btnZatvori;
+
+    public void akcijaZatvaranja(ActionEvent actionEvent) {
+        Stage stage =(Stage)btnZatvori.getScene().getWindow();
+        stage.close();
+    }
 }
