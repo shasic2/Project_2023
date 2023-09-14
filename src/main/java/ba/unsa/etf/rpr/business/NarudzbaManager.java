d621c1ab63cf553b9bec486a8fe1e95c61751655
shasic2 <shasic2@etf.unsa.ba>
2023-09-15 01:30:53 +0200

    Java Doc dokumentacija u NarudzbaManager

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
@@ -4,24 +4,30 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Narudzba;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
+import java.util.Collections;
 import java.util.List;
+/**
+ * Sloj biznis logike za upravljanje narudzbama
+ *
+ * @author Selma
+ */
 
 public class NarudzbaManager {
 
     public void obrisiNarudzbu(int id) throws HealthyShopException {
-        DaoFactory.NarudzbaDao().delete(id);
+        DaoFactory.narudzbaDao().delete(id);
     }
 
     public void dodajNarudbu(Narudzba narudzba) throws HealthyShopException {
-        DaoFactory.NarudzbaDao().add(narudzba);
+        DaoFactory.narudzbaDao().add(narudzba);
     }
 
 
     public void azurirajNarudzbu(Narudzba narudzba) throws HealthyShopException {
-        DaoFactory.NarudzbaDao().update(narudzba);
+        DaoFactory.narudzbaDao().update(narudzba);
     }
 
     public List<Object> dajSveNarudzbe() throws HealthyShopException {
-        return DaoFactory.NarudzbaDao().getAll();
+        return Collections.singletonList(DaoFactory.narudzbaDao().getAll());
     }
 }
