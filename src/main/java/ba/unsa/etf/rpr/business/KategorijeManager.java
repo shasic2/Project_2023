325e10b0b8770622040b23144200c66fdc5d70fc
shasic2 <shasic2@etf.unsa.ba>
2023-09-15 01:29:03 +0200

    Java Doc dokumentacija u KategorijaManager

--- src/main/java/ba/unsa/etf/rpr/business/KategorijeManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KategorijeManager.java
@@ -5,7 +5,11 @@ import ba.unsa.etf.rpr.domain.Kategorije;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 import java.util.List;
-
+/**
+ * Sloj biznis logike za upravljanje kategorijama
+ *
+ * @author Selma
+ */
 public class KategorijeManager {
     public void obrisiKategoriju(int id) throws HealthyShopException {
         DaoFactory.kategorijeDao().delete(id);
@@ -22,7 +26,5 @@ public class KategorijeManager {
 
     }
 
-    public Kategorije pronadjiKategorijuPoID(int id) throws HealthyShopException {
-        return DaoFactory.kategorijeDao().getById(id);
-    }
+
 }
