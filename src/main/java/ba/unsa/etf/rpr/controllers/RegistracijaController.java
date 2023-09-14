85ef1560b26a29ee0bf651d70e10591b1f635302
shasic2 <shasic2@etf.unsa.ba>
2023-09-14 23:02:10 +0200

    dodano da Textfield promjeni boju ukoliko dodje do greske

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistracijaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistracijaController.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.KorisnikManager;
+import ba.unsa.etf.rpr.domain.Korisnik;
+import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,9 +11,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
@@ -23,7 +33,118 @@ public class RegistracijaController {
     public Label Labela1, Labela2, Labela3, Labela4, Labela5;
     public Button btnPomoc, btnONama, btnZatvori, btnRegistracija;
 
-    public void akcijaRegistracije(ActionEvent actionEvent) {
+    KorisnikManager korisnikManager = new KorisnikManager();
+
+    public void akcijaRegistracije(ActionEvent actionEvent) throws HealthyShopException {
+
+        List<Korisnik> listaRegKorisnika = korisnikManager.dajSveKorisnike();
+
+        String unesenoIme = idIme.getText();
+        String unesenoPrezime = idPrezime.getText();
+        String unesenaAdresa = idAdresa.getText();
+        String uneseniEmail = idEmail.getText();
+        String uneseniPassword = idPassword.getText();
+
+        int brojac = 0, brojac1 = 0;
+        if (Objects.equals(unesenoIme, "")) {
+            Labela1.setText("Polje ne može biti prazno !");
+            brojac++;
+            idIme.getStyleClass().add("errorField");
+        } else {
+            Labela1.setText("");
+            idIme.getStyleClass().remove("errorField");
+        }
+        if (Objects.equals(unesenoPrezime, "")) {
+            Labela2.setText("Polje ne može biti prazno !");
+            brojac++;
+            idPrezime.getStyleClass().add("errorField");
+        } else {
+            Labela2.setText("");
+            idPrezime.getStyleClass().remove("errorField");
+        }
+        if (Objects.equals(unesenaAdresa, "")) {
+            Labela3.setText("Polje ne može biti prazno !");
+            brojac++;
+            idAdresa.getStyleClass().add("errorField");
+        } else {
+            Labela3.setText("");
+            idAdresa.getStyleClass().remove("errorField");
+        }
+        if (Objects.equals(uneseniEmail, "")) {
+            Labela4.setText("Polje ne može biti prazno !");
+            brojac++;
+            idEmail.getStyleClass().add("errorField");
+        } else {
+            for (Korisnik k : listaRegKorisnika) {
+                if (k.getEmail().equals(uneseniEmail)) {
+                    brojac++; //ako postoji vec isti email
+                    brojac1++;
+                    Labela4.setText("Email već postoji !");
+                    idEmail.getStyleClass().add("errorField");
+                }
+            }
+            if (brojac1 == 0)
+                Labela4.setText("");
+            idEmail.getStyleClass().remove("errorField");
+        }
+
+
+        if (Objects.equals(uneseniPassword, "")) {
+            Labela5.setText("Polje ne može biti prazno !");
+            brojac++;
+            idPassword.getStyleClass().add("errorField");
+        } else if (idPassword.getText().length() < 6) {
+            Labela5.setText("Password mora sadržavati 6 karaktera !");
+            brojac++;
+            idPassword.getStyleClass().add("errorField");
+        }
+        else {
+            Labela5.setText("");
+            idPassword.getStyleClass().remove("errorField");
+        }
+
+        if (brojac == 0 && brojac1 == 0) {
+            Korisnik k = new Korisnik();
+            k.setIme(unesenoIme);
+            k.setPrezime(unesenoPrezime);
+            k.setAdresa(unesenaAdresa);
+            k.setEmail(uneseniEmail);
+            k.setSifra(uneseniPassword);
+
+            try {
+                FileReader r = new FileReader("src/main/resources/database.properties");
+                Properties p = new Properties();
+                p.load(r);
+                String string1 = p.getProperty("db.server");
+                String string2 = p.getProperty("db.username");
+                String string3 = p.getProperty("db.password");
+
+                Connection connection = DriverManager.getConnection(string1, string2, string3);
+                System.out.println(string1 + " " + string2 + " " + string3);
+
+                Korisnik noviK = korisnikManager.dodajKorisnika(k);
+
+                try {
+                    Stage stage = (Stage) btnRegistracija.getScene().getWindow();
+                    stage.close();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/potvrdaR.fxml"));
+                    fxmlLoader.setController(new PotvrdaRegistracijeController());
+                    Parent root = fxmlLoader.load();
+                    stage.setScene(new Scene(root, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+                    stage.setResizable(false);
+                    stage.show();
+
+                } catch (Exception e1) {
+                    System.out.println(e1.getMessage());
+                }
+                connection.close();
+
+            }catch (Exception e2) {
+                e2.printStackTrace();
+            }
+
+
+        }
     }
 
     public void akcijaZatvaranja(ActionEvent actionEvent) {
