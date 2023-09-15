6536d2129112f9c97788f325038deae81ad7f62c
shasic2 <shasic2@etf.unsa.ba>
2023-09-15 02:07:39 +0200

    Java Doc dokumentacija za dao klasu narudzbaProizvoda

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaProizvodaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaProizvodaDao.java
@@ -5,6 +5,11 @@ import ba.unsa.etf.rpr.domain.NarudzbaProizvoda;
 import ba.unsa.etf.rpr.exceptions.HealthyShopException;
 
 import java.util.List;
+/**
+ * DAO interfejs za objekat NarudzbaProizvod
+ *
+ * @author Selma
+ */
 
 public interface NarudzbaProizvodaDao extends Dao<NarudzbaProizvoda> {
     List<NarudzbaProizvoda> dajPoKorisniku(int id_korisnik) throws HealthyShopException;
