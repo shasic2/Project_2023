cc9f0122819cd84224840880afd0834f0d20a1c1
shasic2 <shasic2@etf.unsa.ba>
2023-09-15 02:01:21 +0200

    Java Doc dokumentacija u controlleru potvrde registracije

--- src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaRegistracijeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaRegistracijeController.java
@@ -4,6 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * JavaFX controller za kreiranje i izmjenu prozora potvrde korisnikove registracije
+ *
+ * @author Selma
+ */
+
 public class PotvrdaRegistracijeController {
     public Button btnZatvori;
 
